1054fab7a6b6c0e4205dec052a823861ba58b348
tctienn <mainichichimvm@gmail.com>
2024-04-23 19:06:09 +0700

    modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import Home from '../views/Home.vue'
 import DetailProDuct from '../views/DetailProduct.vue';
 import ErroPage from '@/views/ErroPage.vue'
@@ -10,66 +9,56 @@ import CartUser from '@/views/CartUser.vue';
 import PayVNPay from '@/views/PayVNPay.vue';
 import Profile from '@/views/Profile.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'homes',
+    component: Home
+  },
+  {
+    path: '/product/detail/:id',
+    name: 'product',
+    component: DetailProDuct
+  },
+  {
+    path: '/blog/detail/:id',
+    name: 'blog',
+    component: DetailBlog
+  },
+  {
+    path: '/collecttion/',
+    name: 'collecttion',
+    component: collecttion
+  },
+  {
+    path: '/login',
+    name: 'logins',
+    component: () => import('@/views/login.vue')
+  },
+  {
+    path: '/cart-user',
+    name: 'cart',
+    component: CartUser
+  },
+  {
+    path: '/pay',
+    name: 'pay',
+    component: PayVNPay
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: ErroPage
+  }
+]
 
-const routes = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    {
-      path: '/',
-      name: 'homes',
-      component: Home
-    },
-    {
-      path: '/product/detail/:id',
-      name: 'product',
-      component: DetailProDuct
-    },
-    {
-      path: '/blog/detail/:id',
-      name: 'blog',
-      component: DetailBlog
-    },
-    {
-      path: '/collecttion/',
-      name: 'collecttion',
-      component: collecttion
-    },
-    {
-      path: '/login',
-      name: 'logins',
-      component: () => import('@/views/login.vue')
-    },
-    {
-      path: '/cart-user',
-      name: 'cart',
-      component: CartUser
-    },
-    {
-      path: '/pay',
-      name: 'pay',
-      component: PayVNPay
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: Profile
-    },
-
-
-
-    {
-      path: '/:pathMatch(.*)*',
-      component: ErroPage
-    } // lỗi đườn dẫn,
-  ]
-})
 const router = createRouter({
   history: createWebHistory(),
   routes: routes
 })
+
 export default router
