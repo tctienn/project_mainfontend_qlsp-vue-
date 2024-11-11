8307270cf84239e96a22297cdbcbf9f3ad39cf52
tctienn <mainichichimvm@gmail.com>
2024-11-11 20:30:44 +0700

    fix ip ssl

--- src/api/CookieFuntion.js
+++ src/api/CookieFuntion.js
@@ -1,7 +1,7 @@
 import { toast } from "vue3-toastify";
 
 // export const domain = 'https://backendqlspayay.up.railway.app'
-export const domain = 'https://tcds.io.vn:8080'
+export const domain = 'https://tcds.io.vn'
 
 // const name ='login_token';
 export const taocock = (name, token) => {
